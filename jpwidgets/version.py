d128ace6a076924eea8fae41ef68685586042b1d
Wolfgang Fahl <wf@bitplan.com>
2022-05-15 15:25:15 +0200

    prepare release

--- jpwidgets/version.py
+++ jpwidgets/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyJustpyWidgets
     '''
     name="pyJustpyWidgets"
-    version='0.0.1'
+    version='0.0.2'
     date = '2022-05-15'
     updated = '2022-05-15'
     description='justpy Widgets'
