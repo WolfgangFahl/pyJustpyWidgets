f8e7bd929f1d6be96e1aa1d4989fb3f454508535
Wolfgang Fahl <wf@bitplan.com>
2022-05-15 08:48:55 +0200

    adds first demo

---
+++ jpdemo/eventdemo.py
@@ -0,0 +1,15 @@
+import justpy as jp
+
+def my_click(self, msg):
+    print(msg)
+    self.text = 'I was clicked'
+
+def event_demo():
+    wp = jp.WebPage()
+    wp.debug = True
+    d = jp.Div(text='Not clicked yet', a=wp, classes='w-48 text-xl m-2 p-1 bg-blue-500 text-white')
+    d.on('click', my_click)
+    d.additional_properties =['screenX', 'pageY','altKey','which','movementX','button', 'buttons']
+    return wp
+
+jp.justpy(event_demo,port=10001)
