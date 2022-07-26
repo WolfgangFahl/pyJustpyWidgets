0471fe4cd312b6259b2e448f636040e69834c217
Wolfgang Fahl <wf@bitplan.com>
2022-07-26 13:54:16 +0200

    fixes #8

--- jpwidgets/version.py
+++ jpwidgets/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyJustpyWidgets
     '''
     name="pyJustpyWidgets"
-    version='0.0.8'
+    version='0.0.9'
     date = '2022-05-15'
     updated = '2022-07-26'
     description='justpy Widgets'
