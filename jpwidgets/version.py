ef6bd626540303553505169eb4a319c3dd12b686
tholzheim <tim.holzheim@rwth-aachen.de>
2022-07-28 09:19:09 +0200

    prepares v0.0.13

--- jpwidgets/version.py
+++ jpwidgets/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyJustpyWidgets
     '''
     name="pyJustpyWidgets"
-    version='0.0.12'
+    version='0.0.13'
     date = '2022-05-15'
-    updated = '2022-07-27'
+    updated = '2022-07-28'
     description='justpy Widgets'
