f8fd152cc93a6a9cd6bc03d94e236cac932dab05
tholzheim <tim.holzheim@rwth-aachen.de>
2022-08-04 11:15:30 +0200

    prepares v0.0.19

--- jpwidgets/version.py
+++ jpwidgets/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyJustpyWidgets
     '''
     name="pyJustpyWidgets"
-    version='0.0.18'
+    version = '0.0.19'  # Maybe rename to number?
     date = '2022-05-15'
-    updated = '2022-08-03'
+    updated = '2022-08-04'
     description='justpy Widgets'
