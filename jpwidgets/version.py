5ed4084f094c5f16c50f1f4b0578a46740b27fc1
Wolfgang Fahl <wf@bitplan.com>
2022-08-30 18:54:53 +0200

    upgrades justpy

--- jpwidgets/version.py
+++ jpwidgets/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyJustpyWidgets
     '''
     name="pyJustpyWidgets"
-    version = '0.0.24'  # Maybe rename to number?
+    version = '0.0.25'  # Maybe rename to number?
     date = '2022-05-15'
-    updated = '2022-08-17'
+    updated = '2022-08-30'
     description='justpy Widgets'
