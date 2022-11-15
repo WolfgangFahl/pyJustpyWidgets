190261aa8bc3cb2bd0b95cbd5484bea752b6f040
Wolfgang Fahl <wf@bitplan.com>
2022-11-15 08:15:33 +0100

    tries fixing lod loading

--- jpwidgets/version.py
+++ jpwidgets/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyJustpyWidgets
     '''
     name="pyJustpyWidgets"
-    version = '0.0.29'  # Maybe rename to number?
+    version = '0.1.0'  # Maybe rename to number?
     date = '2022-05-15'
-    updated = '2022-11-04'
+    updated = '2022-11-15'
     description='justpy Widgets'
